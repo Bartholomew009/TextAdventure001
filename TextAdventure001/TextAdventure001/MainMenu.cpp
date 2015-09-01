859deb5c6c588651a6fe11683b8893274b9cdd36
reverett93@live.com <ganondor0f6>
2015-09-01 18:56:24 -0500

    Created MainMenu Class

---
+++ TextAdventure001/TextAdventure001/MainMenu.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using std::cout;
+class MainMenu {
+
+
+	MainMenu(){
+		std::cout << "This game be swag yo.\n";
+	}
+};
---
+++ TextAdventure001/TextAdventure001/Source.cpp
@@ -0,0 +1,14 @@
+//Ryan Everett (c) September 1, 2015
+//Created September 1, 2015
+//Main source file
+//Last Updated: September 1, 2015
+
+int main(int argc, char** argv) {
+
+
+
+
+
+
+	return 0;
+}
