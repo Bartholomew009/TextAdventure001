c65285574a847637df7307cd4eba96475da7ac3e
bartholomew009 <reveret93@live.com>
2015-09-26 13:08:22 -0500

    Created teh menu header file ...again

---
+++ TextAdventure001/TextAdventure001/MainMenu.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using std::cout;
+using std::cin;
+static class MainMenu {
+public:
+	MainMenu();
+	void MenuChoice();
+	void MenuOptions();
+	void MenuLoad();
+	void MenuNew();
+private:
+	char c;
+
+};
